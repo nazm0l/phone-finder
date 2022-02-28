e454716e17b9211be98f19f7bfdb5f756ae836e3
nazm0l <tanjirahmednazmul@gmail.com>
2022-02-28 21:26:01 +0600

    succesfully fetch data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+const allPhone = () => {
+  const searchValue = document.getElementById("searchInput").value;
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
 
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayPhone(data.data));
+};
+
+const displayPhone = (phones) => {
+  phones.forEach((phone) => {
+    console.log(phone.phone_name);
+  });
+};
