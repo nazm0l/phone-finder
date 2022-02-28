e34faec05b339960dd5a9d66151b390499bc728c
nazm0l <tanjirahmednazmul@gmail.com>
2022-02-28 23:29:42 +0600

    successfully displayed data

--- js/app.js
+++ js/app.js
@@ -8,7 +8,22 @@ const allPhone = () => {
 };
 
 const displayPhone = (phones) => {
+  const phoneContainer = document.getElementById("phone-container");
   phones.forEach((phone) => {
-    console.log(phone.phone_name);
+    console.log(phone);
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
+        <div class="card h-100">
+        <img src="${phone.image}" class="card-img-top w-50 mx-auto p-4" alt="...">
+        <div class="card-body">
+        <h4 class="card-title">Name: ${phone.phone_name}</h4>
+        <h6>Brand: ${phone.brand}</h6>
+        <p class="card-text"></p>
+        <button type="button" class="btn btn-success">Show details</button>
+        </div>
+        </div>
+    `;
+    phoneContainer.appendChild(div);
   });
 };
